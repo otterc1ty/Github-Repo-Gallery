ffc2bfe8e31ff5eb124ac87bece555e506d96e73
leah_b <brsleah27@gmail.com>
2025-07-18 13:03:06 -0400

    Removed duplicate code

--- js/script.js
+++ js/script.js
@@ -89,34 +89,4 @@ const displayRepoInfo = function (repoInfo, languages) { //this function display
     <p>Languages: ${languages.join(", ")}</p>
     <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>`
     repoData.append(div);
-}  
-
-//^^We are using repoInfo in the {} brackets because this is the function using JSON data to grab relevant properties to display on the page
-
-const overview = document.querySelector(".overview") //targets the div of where your profile information will appear
-const username = "otterc1ty";
-
-const gitUserInfo = async function () {
-    const userInfo = await fetch(`https://api.github.com/users/${username}`);
-    const data = await userInfo.json();
-    displayUserInfo(data) //calls the function displaying the user information
 };
-
-gitUserInfo();
-
-const displayUserInfo = function (data) {
-    const div = document.createElement("div"); //creating a new div
-    div.classList.add("user-info"); 
-    div.innerHTML = `
-    <figure>
-    <img alt="user avatar" src=${data.avatar_url} />
-    </figure>
-    <div>
-    <p><strong>Name:</strong> ${data.name}</p>
-    <p><strong>Bio:</strong> ${data.bio}</p>
-    <p><strong>Location:</strong> ${data.location}</p>
-    <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
-    </div>
-    `;
-    overview.append(div)
-}
